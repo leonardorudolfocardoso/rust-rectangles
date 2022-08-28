d2cdeb2a372e6003dcd472dca08129067ffcd4e1
Leonardo C <leonardorudolfocardoso@gmail.com>
2022-08-28 20:50:54 -0300

    rectangle square impl

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,12 @@ fn main() {
         width: 60,
         height: 45,
     };
+    let rect4 = Rectangle::square(90);
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("rect1 is a square? {}", rect1.isSquare());
+    println!("rect4 is a square? {}", rect4.isSquare());
 }
 
 
@@ -30,5 +33,16 @@ impl Rectangle {
 
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.height > other.height && self.width > other.width
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn isSquare(&self) -> bool {
+        self.height == self.width
     }
 }
